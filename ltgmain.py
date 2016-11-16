095d436f06be5523ad9e310d7538122c4d165908
John Mayor Curley <mayorcurley@hotmail.com>
2016-11-16 05:12:08 -0500

    added main python file

---
+++ ltgmain.py
@@ -0,0 +1,34 @@
+import sys
+import londontubegraph as ltg
+
+def main(argv = None):
+    if argv is None:
+        argv = sys.argv
+
+    station_input = ''
+    
+    while station_input != 'exit':
+
+        station_input = input('What station would you like to start from? (type exit to end program)  ')
+        step_input = input('How many steps from the station would you like to go?  (positive numbers only)  ')
+
+        if station_input != 'exit' and step_input:
+            london_tube = ltg.LondonTube()
+
+            print('')
+            print('')
+            if int(step_input) >= 0:
+                print('==========================================================================')
+                print('Tubes just steps away')
+                print('==========================================================================')
+
+            london_tube.printNeighbors(str(station_input), int(step_input))
+
+            print('')
+            print('')
+    
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
